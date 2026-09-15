5f0796a0b36a004f60b3cf9a4f97e50e98056acc
Geovanni <geovannicalado18@gmail.com>
2026-09-15 14:42:14 -0300

    Alterações no banco de dados

--- database/custosVision.sql
+++ database/custosVision.sql
@@ -1,20 +1,20 @@
-CREATE DATABASE CustosVision;
-USE CustosVision;
- 
+CREATE DATABASE CustusVision;
+USE CustusVision;
+
 CREATE TABLE usuario(
 	id_usuario INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	nome VARCHAR(70) NOT NULL,
 	email VARCHAR(150) NOT NULL UNIQUE,
 	senha VARCHAR(70) NOT NULL CHECK (CHAR_LENGTH(senha) > 3)
 );
- 
+
 CREATE TABLE categoria(
 	id_categoria INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE,
 	nome VARCHAR(70) NOT NULL UNIQUE,
-	id_usuario INTEGER,
+	id_usuario INTEGER NOT NULL,
 	FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario)
 );
- 
+
 CREATE TABLE renda(
 	id_renda INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE,
 	descricao VARCHAR(150),
@@ -22,10 +22,10 @@ CREATE TABLE renda(
 	tipo_renda VARCHAR(70) NOT NULL,
 	periodicidade VARCHAR(50),
 	datas DATE NOT NULL,
-	id_usuario INTEGER,
+	id_usuario INTEGER NOT NULL,
 	FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario)
 );
- 
+
 CREATE TABLE despesa(
 	id_despesa INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE,
 	descricao VARCHAR(150),
@@ -33,26 +33,26 @@ CREATE TABLE despesa(
 	periodicidade VARCHAR(50),
 	datas DATE NOT NULL,
 	valor DECIMAL(10,2) NOT NULL CHECK(valor > 0),
-	id_usuario INTEGER,
+	id_usuario INTEGER NOT NULL,
 	FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario),
 	id_categoria INTEGER,
 	FOREIGN KEY (id_categoria) REFERENCES categoria (id_categoria)
 );
- 
+
 CREATE TABLE meta(
 	id_meta INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE,
 	nome VARCHAR(150) NOT NULL UNIQUE,
 	valor_objetivo DECIMAL(10,2) NOT NULL CHECK(valor_objetivo > 0),
 	prazo DATE NOT NULL,
 	status VARCHAR(50) NOT NULL,
-	id_usuario INTEGER,
+	id_usuario INTEGER NOT NULL,
 	FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario)
 );
- 
+
 CREATE TABLE aporte_meta(
 	id_aporte INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE,
 	valor DECIMAL(10,2) NOT NULL CHECK(valor > 0),
 	datas DATE NOT NULL,
-	id_meta INTEGER,
+	id_meta INTEGER NOT NULL,
 	FOREIGN KEY (id_meta) REFERENCES meta (id_meta)
 );
